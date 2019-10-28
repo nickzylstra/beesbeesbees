1ee67eb8ca79d87c638b385981255d3a2f19d106
nickzylstra <nick.zylstra@gmail.com>
2019-10-28 13:37:24 -0700

    Completes Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-var Bee = function() {
+const Bee = function Bee() {
+  // eslint-disable-next-line no-undef
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+// eslint-disable-next-line no-undef
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,9 @@
-var Grub = function() {
+const Grub = function Grub() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = (food) => {
+  console.log(`I ate the ${food}`);
+};
